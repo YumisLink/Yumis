c4ff97b7e7666964e9799388898f552f5ac6d245
YumisLink <57590359+YumisLink@users.noreply.github.com>
2020-05-05 03:08:18 +0800

    111

---
+++ Game/Assets/Scenes/sprite/Manager/ManagerVars.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CreateManagerVarsContainer")]
+public class Manager : ScriptableObject
+{
+    public GameObject[,] PlaceRole = new GameObject[7, 10];
+
+}
---
+++ Game/Assets/Scenes/sprite/NormalAttackMissile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalAttackMissile : MonoBehaviour
+{
+    /// <summary>
+    /// 攻击间隔
+    /// </summary>
+    public float AttackInterval;
+
+    /// <summary>
+    /// 时间计算(不可修改)
+    /// </summary>
+    private float time;
+
+    void Start()
+    {
+        time = AttackInterval;
+    }
+
+    void Update()
+    {
+        time++;
+        if (time >= AttackInterval)
+        {
+            GameObject go = Instantiate(S)
+        }
+    }
+}
